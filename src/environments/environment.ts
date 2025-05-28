b42f89ede93b301d12a9b23af02bf67f9da41023
QuveChoi <ybchoi@quve.kr>
2025-05-28 09:00:57 +0900

    초기화 방식 단순화 적용

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 export const environment = {
   firebase: {
-    apiKey: 'API_KEY',
-    authDomain: 'PROJECT_ID.firebaseapp.com',
-    databaseURL: 'https://PROJECT_ID.firebaseio.com',
-    projectId: 'PROJECT_ID',
-    storageBucket: 'PROJECT_ID.appspot.com',
-    messagingSenderId: 'SENDER_ID',
-    appId: 'APP_ID',
-    measurementId: 'G-MEASUREMENT_ID',
-  },
+    apiKey: "AIzaSyBAjZIJobO3KpIYuQMJ5D9y6Wwfvvk-OHE",
+    authDomain: "friendlychat-15567.firebaseapp.com",
+    projectId: "friendlychat-15567",
+    storageBucket: "friendlychat-15567.firebasestorage.app",
+    messagingSenderId: "511919141787",
+    appId: "1:511919141787:web:dd00faffe2469053fce73e"
+  }
 };
+
+// Initialize Firebase
+const app = initializeApp(environment.firebase);
